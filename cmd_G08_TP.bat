0a09de8233592a650441f2dd144387c8421bce5a
GunCodarith <guncodarith@gmail.com>
2025-09-16 14:02:04 +0700

    Update cmd_G08_TP.bat

--- cmd_G08_TP.bat
+++ cmd_G08_TP.bat
@@ -1,8 +1,9 @@
 @echo off
 REM สั่งไม่ให้แสดงคำสั่งรก ๆ บนจอ
-python "%~dp0Vending_Machine_G08.py"
+python "%~dp0MainProg.py"
 REM รันไฟล์ Python ที่อยู่ path เดียวกับ .bat
 pause
 REM ไว้กันหน้าต่าง cmd เด้งปิดเร็วเกินไป
+
 
 
